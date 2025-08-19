39ccb3b3963c32caffdeb74d4853f8c4c4a862c0
daengae <dladmsxor434@naver.com>
2025-08-19 10:44:56 +0900

    feat: 검색 기능 추가

--- EduConnect/src/main/java/com/BugJava/EduConnect/freeboard/repository/FbPostRepository.java
+++ EduConnect/src/main/java/com/BugJava/EduConnect/freeboard/repository/FbPostRepository.java
@@ -3,14 +3,15 @@ package com.BugJava.EduConnect.freeboard.repository;
 import com.BugJava.EduConnect.freeboard.domain.FbPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor; // JpaSpecificationExecutor 임포트
 
 import java.util.List;
 import java.util.Optional;
 
-public interface FbPostRepository extends JpaRepository<FbPost, Long> {
+public interface FbPostRepository extends JpaRepository<FbPost, Long>, JpaSpecificationExecutor<FbPost> {
     // 필요 시 검색 기능 추가
 
-    @EntityGraph(attributePaths = {"user", "comments.user"})
+    @EntityGraph(attributePaths = {"user", "comments", "comments.user"}) // comments.user 추가
     Optional<FbPost> findWithCommentsById(Long id);
 
     @EntityGraph(attributePaths = "user")
