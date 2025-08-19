170945744cae5232482d038aed5e9bac452f86bd
daengae <dladmsxor434@naver.com>
2025-08-19 10:45:18 +0900

    feat: DTO 수정

--- EduConnect/src/main/java/com/BugJava/EduConnect/freeboard/dto/FbPostRequest.java
+++ EduConnect/src/main/java/com/BugJava/EduConnect/freeboard/dto/FbPostRequest.java
@@ -16,7 +16,7 @@ public class FbPostRequest {
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
-    @Size(max = 5000, message = "내용은 5000자 이하로 입력해야 합니다.")
+    @Size(max = 1048576, message = "내용은 1MB(1,048,576자)를 초과할 수 없습니다.")
     private String content;
 
     
