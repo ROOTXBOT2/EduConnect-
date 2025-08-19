d55b1db2a519fe8034da7917287b622f696ce019
daengae <dladmsxor434@naver.com>
2025-08-19 10:41:52 +0900

    feat: setter 추가

--- EduConnect/src/main/java/com/BugJava/EduConnect/common/entity/BaseEntity.java
+++ EduConnect/src/main/java/com/BugJava/EduConnect/common/entity/BaseEntity.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter; // Setter 임포트
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,8 +19,10 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity{
     @CreatedDate
     @Column(updatable = false)
+    @Setter // createdAt에 Setter 추가
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Setter // updatedAt에 Setter 추가 (일관성을 위해)
     private LocalDateTime updatedAt;
 }
